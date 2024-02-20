e7d0209019cb2cb18dbe61c0986d1204da487ce1
pawelkomarnickihrme <124041781+pawelkomarnickihrme@users.noreply.github.com>
2024-02-20 20:47:57 +0100

    Add .env file and tailwindcss-animate plugin*** ***Add lib/utils.ts file*** ***Remove .env.example file*** ***Add components.json file*** ***Update dependencies in package.json*** ***Update app/page.tsx to include Table component*** ***Add components/table.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
+import { Table } from "@/components/table";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -23,19 +24,10 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
-
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-        </main>
-      </div>
-
+      <Table />
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
           Powered by{" "}
@@ -45,7 +37,7 @@ export default async function Index() {
             className="font-bold hover:underline"
             rel="noreferrer"
           >
-            Supabase
+            Komar
           </a>
         </p>
       </footer>
